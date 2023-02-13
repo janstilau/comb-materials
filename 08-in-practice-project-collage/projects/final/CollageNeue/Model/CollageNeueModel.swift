fb1a0b6b222e7c23045597d6489b36f1e5f767a4
liuguoqiang <janstilau@gmail.com>
2023-02-13 11:25:58 +0800

    sync

--- 08-in-practice-project-collage/projects/final/CollageNeue/Model/CollageNeueModel.swift
+++ 08-in-practice-project-collage/projects/final/CollageNeue/Model/CollageNeueModel.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Combine
 import UIKit
 import Photos
@@ -41,23 +13,23 @@ class CollageNeueModel: ObservableObject {
   private let images = CurrentValueSubject<[UIImage], Never>([])
   @Published var imagePreview: UIImage?
   let updateUISubject = PassthroughSubject<Int, Never>()
-
+  
   private(set) var selectedPhotosSubject =
-    PassthroughSubject<UIImage, Never>()
-
+  PassthroughSubject<UIImage, Never>()
+  
   func bindMainView() {
     // 1
     images
       .handleEvents(receiveOutput: { [weak self] photos in
         self?.updateUISubject.send(photos.count)
       })
-      // 2
+    // 2
       .map { photos in
         UIImage.collage(images: photos, size: Self.collageSize)
       }
       .assign(to: &$imagePreview)
   }
-
+  
   func add() {
     selectedPhotosSubject = PassthroughSubject<UIImage, Never>()
     let newPhotos = selectedPhotosSubject
@@ -66,25 +38,25 @@ class CollageNeueModel: ObservableObject {
         self.images.value.count < 6
       })
       .share()
-
+    
     newPhotos
       .map { [unowned self] newImage in
-      // 1
+        // 1
         return self.images.value + [newImage]
       }
-      // 2
+    // 2
       .assign(to: \.value, on: images)
-      // 3
+    // 3
       .store(in: &subscriptions)
   }
-
+  
   func clear() {
     images.send([])
   }
-
+  
   func save() {
     guard let image = imagePreview else { return }
-
+    
     // 1
     PhotoWriter.save(image)
       .sink(
@@ -107,7 +79,7 @@ class CollageNeueModel: ObservableObject {
   private lazy var imageManager = PHCachingImageManager()
   private(set) var thumbnails = [String: UIImage]()
   private let thumbnailSize = CGSize(width: 200, height: 200)
-
+  
   func bindPhotoPicker() {
     
   }
@@ -117,7 +89,7 @@ class CollageNeueModel: ObservableObject {
     allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
     return PHAsset.fetchAssets(with: allPhotosOptions)
   }
-
+  
   func enqueueThumbnail(asset: PHAsset) {
     guard thumbnails[asset.localIdentifier] == nil else { return }
     
